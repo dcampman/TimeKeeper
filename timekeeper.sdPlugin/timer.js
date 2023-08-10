782da275b8c659923e5add93cbddfe70f2899f39
David <dcampman@live.com>
2023-08-09 21:00:15 -0400

    aider: - Added `totalTime` and `isPaused` properties to the `Timer` class. - Modified the `start` method to only start the timer if it is currently paused. - Modified the `pause` method to calculate the elapsed time and add it to `totalTime` if the timer is running. - Added a `resume` method to record the current time if the timer is paused. - Modified the `reset` method to clear the start time, total time, and set the timer to paused. - Modified the `getTime` method to return the total time the timer has been running, taking into account whether it is paused or running. - Added a static `pauseAll` method that pauses all timers in the provided object.
    # Aider chat conversation:
    
    USER: can you extend the Timer class to support a pause all feature and repeated pause and resume actions?
    
    ASSISTANT: Sure, I can help with that. We will modify the `timer.js` file in the `timekeeper.sdPlugin` directory. This file will contain the `Timer` class with `start`, `pause`, `resume`, `reset`, and `pauseAll` methods.
    
    Here is the updated `Timer` class:
    
    ```javascript
    timekeeper.sdPlugin\timer.js
    <<<<<<< ORIGINAL
    class Timer {
      constructor() {
        this.startTime = null;
        this.pauseTime = null;
      }
    
      start() {
        this.startTime = Date.now();
        this.pauseTime = null;
      }
    
      pause() {
        if (this.startTime) {
          this.pauseTime = Date.now() - this.startTime;
          this.startTime = null;
        }
      }
    
      reset() {
        this.startTime = null;
        this.pauseTime = null;
      }
    
      getTime() {
        if (this.pauseTime) {
          return this.pauseTime;
        } else if (this.startTime) {
          return Date.now() - this.startTime;
        } else {
          return 0;
        }
      }
    }
    
    module.exports = Timer;
    =======
    class Timer {
      constructor() {
        this.startTime = null;
        this.pauseTime = null;
        this.totalTime = 0;
        this.isPaused = true;
      }
    
      start() {
        if (this.isPaused) {
          this.startTime = Date.now();
          this.isPaused = false;
        }
      }
    
      pause() {
        if (!this.isPaused && this.startTime) {
          this.totalTime += Date.now() - this.startTime;
          this.startTime = null;
          this.isPaused = true;
        }
      }
    
      resume() {
        if (this.isPaused) {
          this.startTime = Date.now();
          this.isPaused = false;
        }
      }
    
      reset() {
        this.startTime = null;
        this.totalTime = 0;
        this.isPaused = true;
      }
    
      getTime() {
        if (this.isPaused) {
          return this.totalTime;
        } else {
          return this.totalTime + (Date.now() - this.startTime);
        }
      }
    
      static pauseAll(timers) {
        for (let timer of Object.values(timers)) {
          timer.pause();
        }
      }
    }
    
    module.exports = Timer;
    >>>>>>> UPDATED
    ```
    
    This updated `Timer` class works as follows:
    - When `start` is called, it records the current time if the timer is paused.
    - When `pause` is called, it calculates the elapsed time since `start` was called and adds it to `totalTime` if the timer is running.
    - When `resume` is called, it records the current time if the timer is paused.
    - When `reset` is called, it clears the start time and total time, and sets the timer to paused.
    - `getTime` returns the total time the timer has been running. If the timer is paused, it returns `totalTime`. If the timer is running, it returns `totalTime` plus the time since `start` was called.
    - `pauseAll` is a static method that pauses all timers in the provided object.
    
    Please note that this `Timer` class now supports pausing and resuming multiple times. The `pauseAll` method requires an object of `Timer` instances as an argument.

--- timekeeper.sdPlugin/timer.js
+++ timekeeper.sdPlugin/timer.js
@@ -2,32 +2,49 @@ class Timer {
   constructor() {
     this.startTime = null;
     this.pauseTime = null;
+    this.totalTime = 0;
+    this.isPaused = true;
   }
 
   start() {
-    this.startTime = Date.now();
-    this.pauseTime = null;
+    if (this.isPaused) {
+      this.startTime = Date.now();
+      this.isPaused = false;
+    }
   }
 
   pause() {
-    if (this.startTime) {
-      this.pauseTime = Date.now() - this.startTime;
+    if (!this.isPaused && this.startTime) {
+      this.totalTime += Date.now() - this.startTime;
       this.startTime = null;
+      this.isPaused = true;
+    }
+  }
+
+  resume() {
+    if (this.isPaused) {
+      this.startTime = Date.now();
+      this.isPaused = false;
     }
   }
 
   reset() {
     this.startTime = null;
-    this.pauseTime = null;
+    this.totalTime = 0;
+    this.isPaused = true;
   }
 
   getTime() {
-    if (this.pauseTime) {
-      return this.pauseTime;
-    } else if (this.startTime) {
-      return Date.now() - this.startTime;
+    if (this.isPaused) {
+      return this.totalTime;
     } else {
-      return 0;
+      return this.totalTime + (Date.now() - this.startTime);
+    }
+  }
+
+  static pauseAll(timers) {
+    for (let timer of Object.values(timers)) {
+      timer.pause();
     }
   }
 }
