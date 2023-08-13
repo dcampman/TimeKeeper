8d0fff8f240b37caa492ac29434344a77cff4ed9
David <dcampman@live.com>
2023-08-13 13:25:01 -0400

    aider: Removed background color feature from the pause all property inspector.
    # Aider chat conversation:
    
    USER: ok, now i think i see another error. In the pause all property inspector js file, i dont think the background feature is implemented properly. I think we should remove the bgColor property fomr the html page as well as the js file. We wouldnt ever need to set the background color of the other actions from the property inspector of the pasue all action.
    
    ASSISTANT: To remove the background color feature from the pause all property inspector, we need to modify the `com.timekeeper.sdPlugin\actions\pauseall\property-inspector\inspector.html` and `com.timekeeper.sdPlugin\actions\pauseall\property-inspector\inspector.js` files.
    
    Here is the edit block for the `inspector.html` file:
    
    ```html
    com.timekeeper.sdPlugin\actions\pauseall\property-inspector\inspector.html
    <<<<<<< HEAD
        <div>
          <label for="bgColor">Background Color:</label>
          <input type="color" id="bgColor" name="bgColor" />
        </div>
    =======
    >>>>>>> updated
    ```
    
    And here is the edit block for the `inspector.js` file:
    
    ```javascript
    com.timekeeper.sdPlugin\actions\pauseall\property-inspector\inspector.js
    <<<<<<< HEAD
        // Event listener for background color change
        document.getElementById('bgColor').addEventListener('change', function() {
            $SD.api.sendToPlugin($SD.uuid, {
                event: 'setPauseAllBgColor',
                payload: {
                    bgColor: this.value
                }
            });
        });
    =======
    >>>>>>> updated
    ```
    
    These changes will remove the background color feature from the pause all property inspector.

--- com.timekeeper.sdPlugin/actions/pauseall/property-inspector/inspector.js
+++ com.timekeeper.sdPlugin/actions/pauseall/property-inspector/inspector.js
@@ -2,15 +2,6 @@ const PropertyInspector = require('../../../libs/js/property-inspector.js');
 const Utils = require('../../../libs/js/utils.js');
 
 $PI.onConnected((jsn) => {
-    // Event listener for background color change
-    document.getElementById('bgColor').addEventListener('change', function() {
-        $SD.api.sendToPlugin($SD.uuid, {
-            event: 'setPauseAllBgColor',
-            payload: {
-                bgColor: this.value
-            }
-        });
-    });
     // Event listener for paused background color change
     document.getElementById('pausedBgColor').addEventListener('change', function() {
         $SD.api.sendToPlugin($SD.uuid, {
